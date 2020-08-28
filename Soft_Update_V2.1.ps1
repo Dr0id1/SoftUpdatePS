56bb52d8ef39501fc9da47a9081cf3109364b06d
Dr0id1 <maxpage312@gmail.com>
2020-08-28 17:42:01 -0400

    Update Soft_Update_V2.1.ps1
    Corrected Invoke-WebRequest

--- Soft_Update_V2.1.ps1
+++ Soft_Update_V2.1.ps1
@@ -13,7 +13,7 @@ Remove-Item RogueKiller64.exe
 Remove-Item RogueKiller32.exe
 Remove-Item AdwCleaner.exe
 cls
-Invoke-FastWebRequest "http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe" -OutFile KVRT.exe 
-Invoke-FastWebRequest "https://download.adlice.com/api/?action=download&app=roguekiller&type=x64" -OutFile RogueKiller64.exe
-Invoke-FastWebRequest "https://download.adlice.com/api/?action=download&app=roguekiller&type=x86" -OutFile RogueKiller32.exe
-Invoke-FastWebRequest "https://download.toolslib.net/download/direct/1/latest?channel=release" -OutFile AdwCleaner.exe
+Invoke-WebRequest "http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe" -OutFile KVRT.exe 
+Invoke-WebRequest "https://download.adlice.com/api/?action=download&app=roguekiller&type=x64" -OutFile RogueKiller64.exe
+Invoke-WebRequest "https://download.adlice.com/api/?action=download&app=roguekiller&type=x86" -OutFile RogueKiller32.exe
+Invoke-WebRequest "https://download.toolslib.net/download/direct/1/latest?channel=release" -OutFile AdwCleaner.exe
