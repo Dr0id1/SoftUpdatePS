656a77e3d5d59fa8350051eda586e3ff568b40ef
Dr0id1 <maxpage312@gmail.com>
2020-08-28 17:31:23 -0400

    Create Soft_Update_V2.1.ps1
    Initial Commit

---
+++ Soft_Update_V2.1.ps1
@@ -0,0 +1,19 @@
+﻿#
+# Script.ps1
+#
+$pshost = get-host
+$pswindow = $pshost.ui.rawui
+$newsize = $pswindow.windowsize
+$newsize.height = 8
+$newsize.width = 70
+$pswindow.windowsize = $newsize
+
+Remove-Item KVRT.exe
+Remove-Item RogueKiller64.exe
+Remove-Item RogueKiller32.exe
+Remove-Item AdwCleaner.exe
+cls
+Invoke-FastWebRequest "http://devbuilds.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe" -OutFile KVRT.exe 
+Invoke-FastWebRequest "https://download.adlice.com/api/?action=download&app=roguekiller&type=x64" -OutFile RogueKiller64.exe
+Invoke-FastWebRequest "https://download.adlice.com/api/?action=download&app=roguekiller&type=x86" -OutFile RogueKiller32.exe
+Invoke-FastWebRequest "https://download.toolslib.net/download/direct/1/latest?channel=release" -OutFile AdwCleaner.exe
